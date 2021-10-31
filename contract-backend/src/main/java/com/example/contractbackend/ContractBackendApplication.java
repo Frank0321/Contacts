7a450e6e2d9e1eaf42c8134790bdb33f27244dbb
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-10-31 20:31:40 +0800

    建立前後端專案

---
+++ contract-backend/src/main/java/com/example/contractbackend/ContractBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.contractbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractBackendApplication.class, args);
+	}
+
+}
