c14a7fb457add88442ef9339ff8359b860bbc7ec
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-11-01 23:30:38 +0800

    設計通訊錄首頁的樣式

--- contacts-frontend/src/views/Contacts.vue
+++ contacts-frontend/src/views/Contacts.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
-    <h1> 通訊錄 </h1>
+    <h1 class="title"> 通  訊  錄 </h1>
+    <div class="container">
+      <table class="table"  cellpadding="10" border='0'>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Name</th>
+            <th>Birthday</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Blair</td>
+            <td>2009-03-15</td>
+            <td>0987654321</td>
+            <td>
+              <button class="edit-btn">Edit</button>
+              <button class="delete-btn">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -11,5 +35,30 @@ export default {
 </script>
 
 <style scoped>
-
+.title{
+  font-size: 40px;
+  font-weight: 900;
+}
+.container{
+  padding: 30px 200px;
+}
+.table{
+  width: 100%;
+  display: table;
+  font-size: 20px;
+}
+td{
+  height: 50px;
+  background-color: white;
+}
+.edit-btn, .delete-btn{
+  background: aquamarine;
+  padding: 8px 20px;
+  margin: 10px;
+  border-radius: 15px;
+  font-weight: bolder;
+}
+.delete-btn{
+  background: orange;
+}
 </style>
