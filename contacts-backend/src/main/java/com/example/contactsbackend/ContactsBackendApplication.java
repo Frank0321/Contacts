3d1b48515b8ff67a8d8f6c865f8ef2d59c3e8768
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2022-01-04 22:51:57 +0800

    merge into master

--- contacts-backend/src/main/java/com/example/contactsbackend/ContactsBackendApplication.java
+++ contacts-backend/src/main/java/com/example/contactsbackend/ContactsBackendApplication.java
@@ -1,32 +1,20 @@
 package com.example.contactsbackend;
 
-import com.example.contactsbackend.contacts.*;
-import com.example.contactsbackend.security.AuditorAwareImpl;
-import com.example.contactsbackend.security.User;
-import com.example.contactsbackend.security.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
+
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-
-import static com.example.contactsbackend.contacts.BloodType.*;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @Slf4j
 public class ContactsBackendApplication {
 
-	@Autowired
-	ContactsMapper contactsMapper;
+//	@Autowired
+//	ContactsMapper contactsMapper;
 
 
 	public static void main(String[] args) {
@@ -79,10 +67,10 @@ public class ContactsBackendApplication {
 //		repository.save(User.builder().userName("test").password("test1234").active(true).roles("ROLE_USER").build());
 //	}
 
-	@Bean
-	public AuditorAware<String> auditorAware(){
-		return new AuditorAwareImpl();
-	}
+//	@Bean
+//	public AuditorAware<String> auditorAware(){
+//		return new AuditorAwareImpl();
+//	}
 
 
 }
